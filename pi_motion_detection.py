557bf9a037b8648e6a7c6ce140b9e7bace2d7de3
Robert Pufky <rpufky@gmail.com>
2022-02-23 14:54:59 -0800

    DetectMotion should be PiMotionDetection class.

--- pi_motion_detection.py
+++ pi_motion_detection.py
@@ -25,7 +25,7 @@ class PiMotionDetection(object):
         motion detection. Default 100.
     DETECT_WIDTH: Integer width of image used to test for motion. Default 128.
     DETECT_HEIGHT: Integer height of image used to test for motion. Default 80.
-  """	  
+  """
   DETECT_THRESHOLD = 10
   DETECT_SENSITIVITY = 100
   DETECT_WIDTH = 128
@@ -47,27 +47,27 @@ class PiMotionDetection(object):
       camera.resolution = (self.DETECT_WIDTH, self.DETECT_HEIGHT)
         with picamera.array.PiRGBArray(camera) as stream:
           camera.exposure_mode = 'auto'
-          camera.awb_mode = 'auto' 
+          camera.awb_mode = 'auto'
           camera.capture(stream, format='rgb')
           return stream.array
 
-def DetectMotion(self):
-  """ Determine if there is motion between two images.
+  def DetectMotion(self):
+    """ Determine if there is motion between two images.
 
-  Returns:
-    Boolean True if motion is 'detected'.
-  """
-  self._diff_image = self._DetectCapture()
-  diff_count = 0
-  for column in range(0, self.DETECT_WIDTH):
-    for row in range(0, self.DETECT_HEIGHT):
-      if (abs(int(self._source_image[row][column][1]) -
-          int(self._diff_image[row][column][1]))
-          > self.DETECT_THRESHOLD):
-        diff_count += 1
-  self._source_image = self._diff_image
-  return diff_count > self.DETECT_SENSITIVITY
+    Returns:
+      Boolean True if motion is 'detected'.
+    """
+    self._diff_image = self._DetectCapture()
+    diff_count = 0
+    for column in range(0, self.DETECT_WIDTH):
+      for row in range(0, self.DETECT_HEIGHT):
+        if (abs(int(self._source_image[row][column][1]) -
+            int(self._diff_image[row][column][1]))
+            > self.DETECT_THRESHOLD):
+          diff_count += 1
+    self._source_image = self._diff_image
+    return diff_count > self.DETECT_SENSITIVITY
 
-            
+
 if __name__ == '__main__':
   pass
